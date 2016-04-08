ee7bd42e236def20c211dd50d2b458f45c8f8996
plok74122 <plok74122@hotmail.com>
2016-04-08 11:37:11 +0800

    新增user_id 讓行事曆可以跟會員系統關聯

--- db/migrate/20160407112411_create_available_sections.rb
+++ db/migrate/20160407112411_create_available_sections.rb
@@ -2,10 +2,13 @@ class CreateAvailableSections < ActiveRecord::Migration
   def change
     create_table :available_sections do |t|
 
+      t.integer :user_id , :index
+
       t.datetime :start
       t.datetime :end
 
       t.timestamps null: false
+
     end
   end
 end
--- db/migrate/20160407113718_create_booked_sections.rb
+++ db/migrate/20160407113718_create_booked_sections.rb
@@ -2,9 +2,11 @@ class CreateBookedSections < ActiveRecord::Migration
   def change
     create_table :booked_sections do |t|
 
+      t.integer :user_id , :index
       t.integer :available_sections_id
 
       t.timestamps null: false
+
     end
   end
 end
